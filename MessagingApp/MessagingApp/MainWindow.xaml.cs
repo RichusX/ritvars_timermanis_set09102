6b67794e1213642dec0fe8b9c1d7a82e88e0ffc4
Timerman <40298561@live.napier.ac.uk>
2017-11-19 23:03:14 +0000

    Added message type detection

--- MessagingApp/MessagingApp/MainWindow.xaml.cs
+++ MessagingApp/MessagingApp/MainWindow.xaml.cs
@@ -24,8 +24,10 @@ namespace MessagingApp
         public string header;
         public string body;
 
-        string headerTwitterPattern = @"@(\w+)";
-        string bodyTwitterPattern = ""; // TODO
+        string headerTwitterPattern = @"^@(\w+)";
+        string headerSmsPattern = @"^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$";
+        string headerEmailPattern = @"^[\w!#$%&'*+\-/=?\^_`{|}~]+(\.[\w!#$%&'*+\-/=?\^_`{|}~]+)*" + "@" + @"((([\-\w]+\.)+[a-zA-Z]{2,4})|(([0-9]{1,3}\.){3}[0-9]{1,3}))$";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -36,10 +38,69 @@ namespace MessagingApp
             header = MsgHeaderBox.Text;
             body = MsgBodyBox.Text;
 
-            Match result = Regex.Match(header, headerTwitterPattern);
-            if (result.Success)
+            Match tweetResult = Regex.Match(header, headerTwitterPattern);
+            Match smsResult = Regex.Match(header, headerSmsPattern);
+            Match emailResult = Regex.Match(header, headerEmailPattern);
+
+            if (tweetResult.Success)
             {
-                MessageBox.Show("Woohoo, that's a twitter tag!");
+
+                if (body.Length > 0 && body.Length <= 140)
+                {
+                    // length is between 1 and 140 ( all gucci! )
+                    MessageBox.Show("Woohoo, that's a valid tweet!");
+                }
+                else
+                {
+                    if (body.Length == 0)
+                    {
+                        MessageBox.Show("Error. Message body cannot be empty!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error. Message body for tweets cannot be longer than 140 characters!");
+                    }
+
+                }
+            }
+            else if (smsResult.Success)
+            {
+                
+                if (body.Length > 0 && body.Length <= 140)
+                {
+                    // length is between 1 and 140 ( all gucci! )
+                    MessageBox.Show("Woohoo, that's a valid SMS!");
+                }
+                else
+                {
+                    if (body.Length == 0){
+                        MessageBox.Show("Error. Message body cannot be empty!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error. Message body for SMS cannot be longer than 140 characters!");
+                    }
+                    
+                }
+            }
+            else if (emailResult.Success)
+            {
+                if (body.Length > 0 && body.Length <= 1028)
+                {
+                    if (MsgSubjectBox.Text.Length > 0 && MsgSubjectBox.Text.Length <= 20)
+                    {
+                        MessageBox.Show("Woohoo, that's a valid email!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error. Invalid subject. Try again!");
+                    }
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Incorrect sender. Try again!");
             }
 
 
@@ -47,8 +108,24 @@ namespace MessagingApp
 
         private void ClearBtn_Click(object sender, RoutedEventArgs e)
         {
-            MsgHeaderBox.Text = ""; // Clear header text box
-            MsgBodyBox.Text = ""; // Clear body text box
+            MsgHeaderBox.Clear();
+            MsgSubjectBox.Clear();
+            MsgBodyBox.Clear();
+        }
+
+        private void MsgHeaderBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Match emailResult = Regex.Match(MsgHeaderBox.Text, headerEmailPattern);
+            if (emailResult.Success)
+            {
+                MsgSubjectBox.Visibility = System.Windows.Visibility.Visible;
+                MsgSubjectLabel.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                MsgSubjectBox.Visibility = System.Windows.Visibility.Hidden;
+                MsgSubjectLabel.Visibility = System.Windows.Visibility.Hidden;
+            }
         }
     }
 }
